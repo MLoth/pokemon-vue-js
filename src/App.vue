0c59c7a5b26727a5ead5300f81a12a1a96053009
MLoth <MLoth@users.noreply.github.com>
2023-11-10 15:13:58 +0100

    feat: cleaned up & project setup

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Pokemon!</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+const getPokemons = async function (limit = 12) {
+  const data = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`,
+  ).then((r) => r.json())
+
+  console.log(data)
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+getPokemons()
+</script>
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+<style lang="scss">
+html {
+  color: #fefefe;
+  background: #303030;
+  font-family: 'Work Sans', sans-serif;
 }
 </style>
