775d69ce7741513136f5169141cead38a973f278
MLoth <MLoth@users.noreply.github.com>
2023-11-11 07:33:29 +0100

    feat: more work in lab

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <h1>Pokemon!</h1>
+  <!-- Deze kan je in App.vue zetten. -->
+  <Logo />
+
+  <Pokemon
+    v-for="pokemon in pokemons"
+    :pokemonData="pokemon"
+    :key="pokemon.name"
+  >
+  </Pokemon>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import Logo from './components/Logo.vue'
+import Pokemon from './components/Pokemon.vue'
+
+const pokemons = ref()
+
 const getPokemons = async function (limit = 12) {
   const data = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=0`,
   ).then((r) => r.json())
 
   console.log(data)
+  pokemons.value = data.results
 }
 
 getPokemons()
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="c-pokemon">
+    <img
+      :src="getPokemonImageUrl()"
+      :alt="`Avatar van ${pokemonData.name}`"
+      class="c-pokemon__image"
+    />
+
+    <div class="c-pokemon__info">
+      <header class="c-pokemon__header">
+        <h2 class="c-pokemon__name">{{ pokemonData.name }}</h2>
+
+        <label class="c-pokemon-spin" for="">
+          <input class="c-pokemon-spin__input" type="checkbox" name="" id="" />
+
+          <RotateCw class="c-pokemon-spin__icon" />
+          <RotateCcw class="c-pokemon-spin__icon" />
+        </label>
+      </header>
+
+      <p class="c-pokemon__info">More info <ChevronRight /></p>
+    </div>
+  </div>
+</template>
+
+<!-- Nu moeten we die data hier nog opvangen! -->
+<script setup>
+import { RotateCcw, RotateCw, ChevronRight } from 'lucide-vue-next'
+
+const props = defineProps({
+  pokemonData: {
+    type: Object, // { }
+    required: true,
+  },
+})
+
+const getPokemonId = function () {
+  const partsOfUrl = props.pokemonData.url.split('/')
+  const pokemonId = partsOfUrl[partsOfUrl.length - 2]
+  return pokemonId
+}
+
+const getPokemonImageUrl = function () {
+  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${getPokemonId()}.gif`
+}
+</script>
