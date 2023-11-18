4362504d4e794e5f795af997979085e750bff625
MLoth <MLoth@users.noreply.github.com>
2023-11-18 09:18:43 +0100

    feat: more labo WIP

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,26 @@
   <!-- Deze kan je in App.vue zetten. -->
   <Logo />
 
-  <Pokemon
-    v-for="pokemon in pokemons"
-    :pokemonData="pokemon"
-    :key="pokemon.name"
-  >
-  </Pokemon>
+  <div class="o-grid">
+    <LoadingSkeletons v-if="loading" :amount="12"></LoadingSkeletons>
+    <Pokemon
+      v-else
+      v-for="pokemon in pokemons"
+      :pokemonData="pokemon"
+      :key="pokemon.name"
+    >
+    </Pokemon>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
 import Logo from './components/Logo.vue'
 import Pokemon from './components/Pokemon.vue'
+import LoadingSkeletons from './components/LoadingSkeletons.vue'
 
 const pokemons = ref()
+const loading = ref(true)
 
 const getPokemons = async function (limit = 12) {
   const data = await fetch(
@@ -24,6 +30,7 @@ const getPokemons = async function (limit = 12) {
 
   console.log(data)
   pokemons.value = data.results
+  loading.value = false
 }
 
 getPokemons()
@@ -34,5 +41,15 @@ html {
   color: #fefefe;
   background: #303030;
   font-family: 'Work Sans', sans-serif;
+}
+
+.o-grid {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: baseline;
+  gap: 2rem;
+  max-width: 72rem;
+  margin: 0 auto;
+  padding: 2rem;
 }
 </style>
--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -10,22 +10,32 @@
       <header class="c-pokemon__header">
         <h2 class="c-pokemon__name">{{ pokemonData.name }}</h2>
 
-        <label class="c-pokemon-spin" for="">
-          <input class="c-pokemon-spin__input" type="checkbox" name="" id="" />
+        <label class="c-pokemon-spin" :for="pokemonData.name">
+          <input
+            class="c-pokemon-spin__input sr-only"
+            type="checkbox"
+            :id="pokemonData.name"
+            v-model="showBack"
+          />
 
-          <RotateCw class="c-pokemon-spin__icon" />
-          <RotateCcw class="c-pokemon-spin__icon" />
+          <span class="sr-only"
+            >Toggle Pokemon from back to front and vice versa.</span
+          >
+
+          <RefreshCcw v-if="showBack" class="c-pokemon-spin__icon" />
+          <RefreshCw v-else class="c-pokemon-spin__icon" />
         </label>
       </header>
 
-      <p class="c-pokemon__info">More info <ChevronRight /></p>
+      <p class="c-pokemon__link">More info <ChevronRight /></p>
     </div>
   </div>
 </template>
 
 <!-- Nu moeten we die data hier nog opvangen! -->
 <script setup>
-import { RotateCcw, RotateCw, ChevronRight } from 'lucide-vue-next'
+import { ref } from 'vue'
+import { RefreshCcw, RefreshCw, ChevronRight } from 'lucide-vue-next'
 
 const props = defineProps({
   pokemonData: {
@@ -34,6 +44,10 @@ const props = defineProps({
   },
 })
 
+const showBack = ref(false)
+// TODO: voorzie een goede focus state bij de checkbox - tip: scss
+// TODO: verberg de originele checkbox - tip: kan beter...
+
 const getPokemonId = function () {
   const partsOfUrl = props.pokemonData.url.split('/')
   const pokemonId = partsOfUrl[partsOfUrl.length - 2]
@@ -41,6 +55,112 @@ const getPokemonId = function () {
 }
 
 const getPokemonImageUrl = function () {
-  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/${getPokemonId()}.gif`
+  const baseUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated`
+
+  if (showBack.value) return `${baseUrl}/back/${getPokemonId()}.gif`
+  return `${baseUrl}/${getPokemonId()}.gif`
 }
 </script>
+
+<style lang="scss">
+.c-pokemon {
+  width: 100%;
+
+  &__info {
+    display: flex;
+    flex-direction: column;
+    background: #444;
+    color: #fff;
+    padding: 0.5rem 1rem;
+    border-radius: 1rem 2rem 1rem 2rem;
+  }
+
+  &__header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__name {
+    font-size: 1.5rem;
+    font-weight: 600;
+    margin: 0;
+
+    &:first-letter {
+      text-transform: uppercase;
+    }
+  }
+
+  &-spin {
+    display: flex;
+    flex-shrink: 0;
+    align-items: center;
+    justify-content: center;
+    background: #111;
+    border-radius: 50%;
+    height: 3rem;
+    width: 3rem;
+    cursor: pointer;
+    margin-left: 1rem;
+    transition: background 0.2s ease-in-out;
+    color: white;
+
+    &:hover {
+      background: #808080;
+    }
+
+    // Als er focus is in de label!
+    &:focus-within {
+      outline: 3px solid lightcyan;
+    }
+  }
+
+  &__image {
+    height: 7rem; // UITZONDERLIJK! Een (vaste) hoogte die groter is dan de image
+    width: auto;
+    display: block;
+    margin-bottom: -0.5rem;
+  }
+
+  &__link {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    padding: 1rem 0;
+    font-size: 0.75rem;
+    margin: 0 0 0 auto; // push to the right
+    text-decoration: underline;
+  }
+}
+
+.sr-only {
+  // sr-only = screenreader only
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border-width: 0;
+}
+
+// TODO: maak de pokemons:
+// vanaf 480px in 2 kolommen
+@media screen and (min-width: 480px) {
+  .c-pokemon {
+    // Tussen de pokemons 1 x 2rem ruimte
+    width: calc((100% - 1 * 2rem) / 2);
+  }
+}
+
+// vanaf 768px in 4 kolommen
+@media screen and (min-width: 768px) {
+  .c-pokemon {
+    // Tussen de pokemons 3 x 2rem ruimte
+    // Uiteindelijk 4 kolommen -> /4
+    width: calc((100% - 3 * 2rem) / 4);
+  }
+}
+</style>
